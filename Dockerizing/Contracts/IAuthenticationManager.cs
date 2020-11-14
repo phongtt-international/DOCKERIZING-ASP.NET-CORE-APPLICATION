115d3c43629ada4a7473cefad3a1b1998cf38a45
phongtt <phongtt.international@gmail.com>
2020-11-14 13:15:47 +0700

    Initial Source with docker

---
+++ Dockerizing/Contracts/IAuthenticationManager.cs
@@ -0,0 +1,11 @@
+﻿using Entities.DataTransferObjects;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValdiateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
+    }
+}
